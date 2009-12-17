cf51fa44ec1a5b60dac6bca11aea0a7853b60bf7
Matt Koscica <mattk@cpan.org>
2009-12-17 14:54:41 +0000

    Import of MATTK/Number-Object-Plugin-Tax-AU-0.05 from CPAN.
    gitpan-cpan-distribution: Number-Object-Plugin-Tax-AU
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         MATTK/Number-Object-Plugin-Tax-AU-0.05.tar.gz
    gitpan-cpan-author:       MATTK
    gitpan-cpan-maturity:     released

--- lib/Number/Object/Plugin/Tax/AU/GST.pm
+++ lib/Number/Object/Plugin/Tax/AU/GST.pm
@@ -5,7 +5,7 @@ use strict;
 
 use base 'Number::Object::Plugin::Tax';
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 our $RATE    = 1.1;
 
 sub calc {
@@ -48,7 +48,7 @@ Number::Object::Plugin::Tax::AU::GST - a Number::Object plugin for Australian GS
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
